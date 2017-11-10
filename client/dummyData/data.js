f31ad7f118a9c5d55034de6296d5cb0b628b350e
Jeff <jeffreyRstocker@gmail.com>
2017-11-10 12:11:32 -0800

    added some dummy data

---
+++ client/dummyData/data.js
@@ -0,0 +1,36 @@
+{[
+  {
+    user: {
+      name: 'bob'
+    }, 
+    locations: [
+      {
+        name: 'home', 
+        lat: 60,
+        long: 60
+      },
+      {
+        name: 'work',
+        lat: 61,
+        long: 61
+      }
+    ]
+  },
+  {
+    user: {
+      name: 'samuel'
+    }, 
+    locations: [
+      {
+        name: 'home', 
+        lat: 72,
+        long: 54
+      },
+      {
+        name: 'work',
+        lat: 54,
+        long: 23
+      }
+    ]
+  }
+]}
