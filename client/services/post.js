3ff9ff11c5d8897210ad0abcc9375a02e349bcf2
Jeff <jeffreyRstocker@gmail.com>
2017-11-11 14:46:17 -0800

    used $.post's promise instead of callback, and fixed promix

--- client/services/post.js
+++ client/services/post.js
@@ -1,12 +1,29 @@
 app.service('$post', function () {
   this.postData = function (dataToSend = {}) {  // need to declare as this
-    $.post('/', JSON.stringify(dataToSend), function (data, status){
-      if (status === 'success') {
+    // console.log((dataToSend))
+    return new Promise ((resolve, reject) => {
+      // $.post('/', JSON.stringify(dataToSend), function (data, status){
+      //   console.log('Status: ', status)
+      //   if (status === 'successful') {
+      //     console.log('post successful')
+      //     console.log('Return Data', data)
+      //     resolve (data)
+      //   } else {
+      //     console.log ('status', status)
+      //     revoke (status)
+      //   }
+      // })
+
+    $.post('/', JSON.stringify(dataToSend))
+      .done((data) => {
         console.log('post successful')
         console.log('Return Data', data)
-      } else {
-        console.log ('status', status)
-      }
+        resolve (data)
+      })
+      .fail((error) => {
+        console.log ('status', error)
+        reject (error)
+      }) 
     })
   }
 })
