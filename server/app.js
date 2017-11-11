ea881a56e1a5cb65d0a3f73a10385a25d9e87450
Jeff <jeffreyRstocker@gmail.com>
2017-11-11 14:48:31 -0800

    added middleware to call data for geo data, and some testing

--- server/app.js
+++ server/app.js
@@ -3,21 +3,32 @@ var app = express();
 var request = require('request')
 var middleware = require('./middleware/middleware.js')
 // var io = require('socket.io')
+var geocoding = require('./api/geocoding.js');
 
 ///// local modules /////
 var db = require('./database.js')
 
+////////// quick testing/////////
+// var address = '944 Market St, San Francisco, CA 94102, USA'
+// var location = '37.7836966,-122.4089664'
+// var id = 'ChIJa2U7loWAhYARXXIPjJLMNcM'
+// geocoding.getAddress(address, null, id)
+//   .then ((data) => {
+//     console.log('address data:', data)
+//   })
+//   .catch((err) => console.log('error') )
 
 ////  user set variables ///////
 var userVar = require("./userVariables.js")
 var port = userVar.port
 
-/////// routing ///////
 
+/////// routing ///////
 app.use(express.static('client'))
 
 app.use('/',middleware.listener)
-app.post('/', middleware.putTogetherBody, db.middleSaveToDatabase, db.middleFindOneInDatabase)
+// app.post('/', middleware.putTogetherBody, db.middleSaveToDatabase, db.middleFindOneInDatabase)
+app.post('/', middleware.putTogetherBody, geocoding.middleRetrieveAddressFromGoogle, db.middleSaveToDatabase, db.middleFindOneInDatabase)
 app.post('/', (req, res) => {
   
   // res.status(201)
