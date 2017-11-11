61e051cd4c456ec76d4a15e5d65b94b9006c0e96
Jeff <jeffreyRstocker@gmail.com>
2017-11-11 14:47:14 -0800

    Fixed bug with catching error in calling $post

--- client/components/userForm.js
+++ client/components/userForm.js
@@ -1,10 +1,19 @@
 angular.module('app').component ('userform', {
-  binding: {},
+  binding: {form: "="},
   controller: function ($post, $scope) {
     $scope.submitDataToServer = function (data) {
       console.log ('data', data)
       $post.postData(data)
+        .then(data => {
+          window.parsePostDataToCreateMarkers(data)
+        })
+        .catch((err) => {
+          console.log ('error with data: ', err)
+        })
     }
+    // $scope.form 
+    // $scope.form.workAddress = '1818 26th Ave, San Francisco, CA 94122, USA'
+    // form.homeAddress = ''
   },
   templateUrl: 'templates/form.html' 
 })
