307180355ee001bca4375d52469a77055a1541fc
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-04 20:31:32 -0700

    added backend passport note still WIP

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const env = require('dotenv').config();
+const path = require('path');
 const port = process.env.PORT || 4040;
 
 var express = require('express');
 var app = express();
 var request = require('request')
 var middleware = require('./middleware/middleware.js')
-// var io = require('socket.io')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var geocoding = require('./api/geocoding.js');
+
+const passport = require ('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+const UserNamePassword = require ('./database/userNamePassword');
 
 ///// local modules /////
 var db = require('./database.js')
@@ -38,30 +45,89 @@ app.post('/', (req, res) => {
   console.log ('POST: sending: req.body');
   res.status(201).send(req.body);
   // res.json(req.body)
-})
+});
 
 // app.get ('/users/data/all', (req, res, next) => {console.log('test'); next()})
 
 app.get ('/users/data/all',db.middleReturnAll, (req, res, next) => {
   // console.log('req.body', req.body)
   res.status(200).send(req.body);
-})
+});
 
 // app.get('/user/location')
 
 app.get((req, res) => {
   res.status(200);
   // res.end('yes')
-})
+});
 
 app.get ('/key', (req, res) => {
   res.send(process.env.BrowserGoogleMapsAPIKey);
-})
+});
+
+// app.get ('/login',  passport.authenticate('local'),
+// function(req, res) {
+//   // If this function gets called, authentication was successful.
+//   // `req.user` contains the authenticated user.
+//   res.redirect('/users/' + req.user.username);
+// });
+
+// app.get('./logout',(req, res) => {
+//   req.logout();
+//   res.end();
+// })
+
+
+// // app.get ('/logout', passport.authenticate('local', { successRedirect: '/')));
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.use('/', routes);
+
+// passport config
+passport.use(new LocalStrategy(UserNamePassword.authenticate()));
+passport.serializeUser(UserNamePassword.serializeUser());
+passport.deserializeUser(UserNamePassword.deserializeUser());
+
+
+
+
+
+
+
+
 
 
 app.listen(port, () => {
   console.log('App is listening on port: ', port);
-})
+});
 
 ///// playing with enviromental Variables
 // console.log ('env', process.env.test)
