c84046bcb4fa1d03c630f80e2d04b2af6f45595c
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-06 23:54:50 -0700

    init mapCtrl

---
+++ client/controllers/mapCtrl.js
@@ -0,0 +1,12 @@
+app.controller('mapCtrl', function ($scope, $http) {
+  $http.get('/key')
+    .then (data => {
+      var key = data.data;
+      var createHiddenAPI = document.createElement('script');
+      createHiddenAPI.src = "https://maps.googleapis.com/maps/api/js?key="+key+"&callback=initGoogle&libraries=places";
+      document.getElementsByTagName('head')[0].appendChild(createHiddenAPI);
+    })
+    .catch (err => {
+      console.log ('Error Retrieving Key', err);
+    })
+});
