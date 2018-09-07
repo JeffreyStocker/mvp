203fa35ef8cef745add81d3e99b5f058414fdaf2
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-06 23:35:35 -0700

    split mapCtrl into its own file

--- client/components/map.js
+++ client/components/map.js
@@ -1,17 +1,7 @@
-
-$.get('/key')
-  .then (key => {
-    var createHiddenAPI = document.createElement('script');
-    createHiddenAPI.src = "https://maps.googleapis.com/maps/api/js?key="+key+"&callback=initGoogle&libraries=places";
-    document.getElementsByTagName('head')[0].appendChild(createHiddenAPI);
-  })
-  .catch (err => {
-    console.log ('Error Retrieving Key', err);
-  })
-
 angular.module("app").component('map', {
   binding: {},
-  controller: 'mainController',
+  // controller: 'mainController',
+  controller: 'mapCtrl',
   template:
   `
   <div id="map" ></div>
