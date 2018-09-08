9e5ecd646c88313dd75eecb3f75b579e275fac0d
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-07 21:26:17 -0700

    removed unneeded code

--- server/app.js
+++ server/app.js
@@ -23,76 +23,30 @@ if (!process.env.BrowserGoogleMapsAPIKey) {
   throw new Error ('A google Javascript api key must be supplied in .env file');
 }
 
-////////// quick testing/////////
-// var address = '944 Market St, San Francisco, CA 94102, USA'
-// var location = '37.7836966,-122.4089664'
-// var id = 'ChIJa2U7loWAhYARXXIPjJLMNcM'
-// geocoding.getAddress(address, null, id)
-//   .then ((data) => {
-//     console.log('address data:', data)
-//   })
-//   .catch((err) => console.log('error') )
-
 
 /////// routing ///////
-
 app.use('/', middleware.listener);
 
 
-// app.post('/', middleware.putTogetherBody, db.middleSaveToDatabase, db.middleFindOneInDatabase)
 app.post('/', middleware.putTogetherBody, geocoding.middleRetrieveAddressFromGoogle, db.middleSaveToDatabase, db.middleFindOneInDatabase);
 app.post('/', (req, res) => {
   console.log ('POST: sending: req.body');
   res.status(201).send(req.body);
-  // res.json(req.body)
 });
 
-// app.get ('/users/data/all', (req, res, next) => {console.log('test'); next()})
 
 app.get ('/users/data/all', db.middleReturnAll, (req, res, next) => {
-  // console.log('req.body', req.body)
   res.status(200).send(req.body);
 });
 
-// app.get('/user/location')
 
 app.get((req, res) => {
   res.status(200);
-  // res.end('yes')
 });
 
 app.get ('/key', (req, res) => {
   res.send(process.env.BrowserGoogleMapsAPIKey);
 });
-
-// app.get ('/login',  passport.authenticate('local'),
-// function(req, res) {
-//   // If this function gets called, authentication was successful.
-//   // `req.user` contains the authenticated user.
-//   res.redirect('/users/' + req.user.username);
-// });
-
-// app.get('./logout',(req, res) => {
-//   req.logout();
-//   res.end();
-// })
-
-
-// // app.get ('/logout', passport.authenticate('local', { successRedirect: '/')));
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function(err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
 
 
 
