915b8bffd2f1d1c199aa1a90903b5f01299efacc
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-07 20:36:49 -0700

    added errors to userLogins

--- client/components/titleBar.js
+++ client/components/titleBar.js
@@ -1,10 +1,10 @@
 angular.module('app').component('titleBar', {
-  binding: {
-    user: '<'
+  bindings: {
+    user: '='
   },
   template: `
   <div class="titleBar">
     <H1 class="floatLeft">THE GREAT CARPOOL PROJECT</H1>
-    <login class="login floatRight" user={{user}}></login>
+    <login class="login floatRight" user="$ctrl.user"></login>
   </div>`
 });
--- client/components/userLogin.js
+++ client/components/userLogin.js
@@ -1,49 +1,64 @@
 angular.module('app').component('login', {
-  binding: {
-    user: '<'
+  bindings: {
+    user: '='
   },
   controller: 'loginController',
   templateUrl: 'templates/userLogin.html'
 }).controller('loginController', function loginController($scope, $http) {
   var ctrl = this;
-  console.log('this works');
-  ctrl.loginInfo = {password: '', username: ''};
+
+
+  var resetErrors = function () {
+    ctrl.error = {general: null, password: null, user: null };
+  };
+  resetErrors();
+
+  var resetLoginInfo = function () {
+    ctrl.loginInfo = {password: '', username: ''};
+  };
+  resetLoginInfo();
 
   ctrl.login = function (user) {
+    resetErrors();
     $http.post('login', user)
       .then((data) => {
-        console.log('data:', data);
+        console.log('login:', data);
+        ctrl.user = data.data.user;
+        resetLoginInfo();
       })
       .catch (err => {
         console.log('err:', err);
-      })
-      .then(() => {
+        if (err.status === 401) {
+          ctrl.error.general = 'Invalid Username or Password';
+        }
         ctrl.loginInfo.password = '';
       });
   };
 
   ctrl.logout = function () {
+    resetErrors();
     $http.get('logout')
       .then(data => {
-
+        console.log('logout:', data);
+        ctrl.user = null;
+        resetLoginInfo();
       })
       .catch (err => {
         console.log('err:', err);
-      })
-      .then(() => {
         ctrl.loginInfo.password = '';
       });
   };
 
   ctrl.create = function (loginInfo) {
+    resetErrors();
     $http.post('register', loginInfo)
       .then(data => {
-
+        console.log('register:', data);
+        ctrl.user = data.data.user;
+        resetLoginInfo();
       })
       .catch (err => {
         console.log('err:', err);
-      })
-      .then(() => {
         ctrl.loginInfo.password = '';
       });
   };
