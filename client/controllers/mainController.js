81e1196c66045bdf288cbcf872b55f4bd58fd060
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-08 21:17:02 -0700

    moved google map api call to mainController

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,5 +1,5 @@
 var app = angular.module('app', [])
-  .controller('mainController', function mainController($scope, $useGet) {
+  .controller('mainController', function mainController($scope, $http) {
   // $scope.name = "Bob" //this works when just using {{name}}
   // this.name = "sdafsdafsdf"  //note this work when specify ng-controller="mainController as ctrl" {{ctrl.name}}
   // this.test2 = {name: '3424324324'}
@@ -11,6 +11,16 @@ var app = angular.module('app', [])
     var ctrl = this;
     ctrl.user = null;
     ctrl.currentView = 'default';
+    $http.get('/key')
+      .then (data => {
+        var key = data.data;
+        var createHiddenAPI = document.createElement('script');
+        createHiddenAPI.src = 'https://maps.googleapis.com/maps/api/js?key=' + key + '&callback=initGoogle&libraries=places';
+        document.getElementsByTagName('head')[0].appendChild(createHiddenAPI);
+      })
+      .catch (err => {
+        console.log ('Error Retrieving Key', err);
+      });
 
   })
   .component('app', {
