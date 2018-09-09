111d044884b6caf2732e266ed66d2cf9e9689242
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-09 08:21:29 -0700

    stripped private user info from Account login

--- server/routeLogin.js
+++ server/routeLogin.js
@@ -4,6 +4,11 @@ var Account = require('./database/userNamePassword');
 var UserDatabase = require ('./database/database');
 var router = express.Router();
 
+var stripUserOfPrivateInfo = function (user) {
+  user.hash = undefined;
+  user.salt = undefined;
+  return user;
+};
 
 router.get('/', function (req, res) {
   console.log('render runs');
@@ -20,29 +25,33 @@ router.post('/register', function(req, res) {
       console.log('err:', err);
       res.status(401).send('User Already Exists');
     } else {
-      console.log (res.body);
-
       passport.authenticate('local')(req, res, function () {
-        res.status(200).send({test: 'test'});
+        res.status(200).send({user: stripUserOfPrivateInfo(req.user)});
       });
     }
   });
 });
 
 router.get('/login', function(req, res) {
-  console.log('login');
+  console.log('login', req.user);
+
+  if (req.user) {
+    return res.status(200).send(stripUserOfPrivateInfo(req.user));
+  }
+  res.status(401).end();
+
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  console.log('post register');
+  console.log('post register', req.user);
 
-  res.status(200).send({user: 'test'});
+  res.status(200).send(stripUserOfPrivateInfo(req.user));
 });
 
 router.get('/logout', function(req, res) {
   console.log('logout');
   req.logout();
-  res.status(200).send({test: 'test'});
+  res.status(200).send();
 });
 
 router.get('/ping', function(req, res) {
