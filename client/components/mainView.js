987006f80bcc12195af4cb14ffd5eeb75e83cc4d
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-13 16:38:23 -0700

    passed user down to view elements

--- client/components/mainView.js
+++ client/components/mainView.js
@@ -1,14 +1,14 @@
 angular.module('app').component('mainView', {
   bindings: {
     currentView: '<',
-    user: '<'
+    user: '='
   },
   template:
   `
   <div ng-switch="$ctrl.currentView">
-    <add-address ng-switch-when="addAddress"></add-address>
-    <userform ng-switch-when="userform"></userform>
-    <list-addresses ng-switch-when="listAddress" user="$ctrl.user"></list-addresses>
+    <add-address ng-switch-when="addAddress" user="$ctrl.user"></add-address>
+    <userform ng-switch-when="userform" user="$ctrl.user"></userform>
+    <list-addresses ng-switch-when="listAddress" user="$ctrl.user" user="$ctrl.user"></list-addresses>
   </div>
   `
 });
