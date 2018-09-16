2103209c9c748f0937381f73f27fd23a25197c19
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-16 09:56:42 -0700

    pass map down to components

--- client/components/mainView.js
+++ client/components/mainView.js
@@ -1,14 +1,15 @@
 angular.module('app').component('mainView', {
   bindings: {
     currentView: '<',
-    user: '='
+    user: '=',
+    map: '='
   },
   template:
   `
   <div ng-switch="$ctrl.currentView">
-    <add-address ng-switch-when="addAddress" user="$ctrl.user"></add-address>
-    <userform ng-switch-when="userform" user="$ctrl.user"></userform>
-    <list-addresses ng-switch-when="listAddress" user="$ctrl.user" user="$ctrl.user"></list-addresses>
+    <add-address ng-switch-when="addAddress" user="$ctrl.user" map="$ctrl.map"></add-address>
+    <userform ng-switch-when="userform" user="$ctrl.user" map="$ctrl.map"></userform>
+    <list-addresses ng-switch-when="listAddress" user="$ctrl.user" user="$ctrl.user" map="$ctrl.map"></list-addresses>
   </div>
   `
 });
