39410502f83b46625496144a37c76c2ee5b8dd0e
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-16 09:57:13 -0700

    converted listViews to use ng-repeat

--- client/components/listViews.js
+++ client/components/listViews.js
@@ -4,16 +4,16 @@ angular.module('app').component('listViews', {
     user: '<'
   },
   controller: 'listViewCtrl',
-  template: `
-  <div class="listViews" ng-if="$ctrl.user">
-    <a ng-click="$ctrl.updateView('addAddress')" ng-class="{selected: $ctrl.currentView === 'addAddress'}">Add Address</a>&nbsp
-    <a ng-click="$ctrl.updateView('listAddress')" ng-class="{selected: $ctrl.currentView === 'listAddress'}">List Addresses</a>&nbsp
-    <a ng-click="$ctrl.updateView('routes')" ng-class="{selected: $ctrl.currentView === 'routes'}">Routes</a>&nbsp
-    <a ng-click="$ctrl.updateView('userform')" ng-class="{selected: $ctrl.currentView === 'userform'}">userform</a>&nbsp
-  </div>
-  `
+  template:
+  `<div class="listViews" ng-if="$ctrl.user">
+    <span  ng-repeat="(key, value) in $ctrl.listOfViews">
+      <a ng-click="$ctrl.updateView(value[1])"
+        ng-class="{selected: $ctrl.currentView === value[1]}">{{value[0]}}</a>&nbsp
+    </span>
+  </div>`
 }).controller('listViewCtrl', function () {
   var ctrl = this;
+  ctrl.listOfViews = [['Add Address', 'addAddress'], ['List Addresses', 'listAddress'], ['Routes', 'routes'], ['UserForms', 'userform']];
   ctrl.updateView = function (newView) {
     ctrl.currentView = newView;
   };
